bec8e63ec97839c30ee890b886c8950fec13f0ec
ganeshthummala48-ship-it <ganeshthummala48@gmail.com>
2026-03-26 07:10:39 +0530

    Initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -61,7 +61,12 @@ const Hero = () => {
                 <path d="M5 12h14M12 5l7 7-7 7"/>
               </svg>
             </a>
-            <a href="#contact" className="btn btn-outline">Get In Touch</a>
+            <a href="/src/assets/resume.pdf" download className="btn btn-outline">
+              Download CV
+              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/>
+              </svg>
+            </a>
           </div>
         </motion.div>
 
